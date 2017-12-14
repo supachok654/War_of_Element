7b5bc100d63fdb9f28373d8cb6e7b3faba6dd647
supachok654 <dome654@hotmail.co.th>
2017-12-14 16:52:47 +0700

    add linked list

--- WarGame/core/src/com/mygdx/game/LinkWaterMinion.java
+++ WarGame/core/src/com/mygdx/game/LinkWaterMinion.java
@@ -1,5 +1,28 @@
 package com.mygdx.game;
 
+import java.util.LinkedList;
+
+import com.badlogic.gdx.Graphics;
+
 public class LinkWaterMinion {
 
+	private LinkedList<WaterMinion> water = new LinkedList<WaterMinion>();
+	
+	WaterMinion  tempWaterMinion;
+	
+	public void tick() {
+		for(int i=0;i<water.size();i++) {
+			tempWaterMinion = water.get(i);
+			
+			tempWaterMinion.tick();
+		}
+	}
+	
+	public void render(Graphics g) {
+		for(int i=0;i<water.size();i++) {
+			tempWaterMinion = water.get(i);
+			
+			tempWaterMinion.render(g);
+		}
+	}
 }
--- WarGame/core/src/com/mygdx/game/WaterMinion.java
+++ WarGame/core/src/com/mygdx/game/WaterMinion.java
@@ -1,5 +1,26 @@
 package com.mygdx.game;
 
-public class WaterMinion {
+import com.badlogic.gdx.Graphics;
 
+public class WaterMinion {
+	
+	private double x;
+	private double y;
+	
+	Water image;
+	
+	public WaterMinion(double x, double y,WarGame game) {
+		this.x = x;
+		this.y = y;
+		
+		image = World.Water(10,70);
+	}
+	
+	public void tick() {
+		x += 10;
+	}
+
+	public void render(Graphics g) {
+		g.drawImage(image,(int)x,(int)y,null);
+	}
 }
